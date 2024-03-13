48eb95c10d2b9861b68609f9a0680a76a733b39e
Leandro Gonçalves de Oliveira <olegon.main+github@gmail.com>
2024-03-12 23:17:16 -0300

    feat: listening to payments requests instead of notifications commands

--- localstack/010-sqs.sh
+++ localstack/010-sqs.sh
@@ -3,4 +3,15 @@
 set -v
 
 awslocal sqs create-queue \
-  --queue-name notificarCliente
+  --queue-name microsservico-cliente-evento-pagamento-aprovado-dlq
+
+awslocal sqs create-queue \
+  --queue-name microsservico-cliente-evento-pagamento-aprovado \
+  --attributes '{ "RedrivePolicy": "{\"deadLetterTargetArn\":\"arn:aws:sqs:us-east-1:000000000000:microsservico-cliente-evento-pagamento-aprovado-dlq\",\"maxReceiveCount\":\"3\"}" }'
+
+awslocal sqs create-queue \
+  --queue-name microsservico-cliente-evento-pagamento-recusado-dlq
+
+awslocal sqs create-queue \
+  --queue-name microsservico-cliente-evento-pagamento-recusado \
+  --attributes '{ "RedrivePolicy": "{\"deadLetterTargetArn\":\"arn:aws:sqs:us-east-1:000000000000:microsservico-cliente-evento-pagamento-recusado-dlq\",\"maxReceiveCount\":\"3\"}" }'
