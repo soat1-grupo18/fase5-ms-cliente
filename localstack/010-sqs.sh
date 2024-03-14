14596f4616b25d26174a9335107df4083ae8b71d
Leandro Gonçalves de Oliveira <olegon.main+github@gmail.com>
2024-03-14 19:31:06 -0300

    chore: rename aws sqs queues;

--- localstack/010-sqs.sh
+++ localstack/010-sqs.sh
@@ -3,15 +3,15 @@
 set -v
 
 awslocal sqs create-queue \
-  --queue-name microsservico-cliente-evento-pagamento-aprovado-dlq
+  --queue-name ms-cliente-evento-pagamento-aprovado-dlq
 
 awslocal sqs create-queue \
-  --queue-name microsservico-cliente-evento-pagamento-aprovado \
-  --attributes '{ "RedrivePolicy": "{\"deadLetterTargetArn\":\"arn:aws:sqs:us-east-1:000000000000:microsservico-cliente-evento-pagamento-aprovado-dlq\",\"maxReceiveCount\":\"3\"}" }'
+  --queue-name ms-cliente-evento-pagamento-aprovado \
+  --attributes '{ "RedrivePolicy": "{\"deadLetterTargetArn\":\"arn:aws:sqs:us-east-1:000000000000:ms-cliente-evento-pagamento-aprovado-dlq\",\"maxReceiveCount\":\"3\"}" }'
 
 awslocal sqs create-queue \
-  --queue-name microsservico-cliente-evento-pagamento-recusado-dlq
+  --queue-name ms-cliente-evento-pagamento-recusado-dlq
 
 awslocal sqs create-queue \
-  --queue-name microsservico-cliente-evento-pagamento-recusado \
-  --attributes '{ "RedrivePolicy": "{\"deadLetterTargetArn\":\"arn:aws:sqs:us-east-1:000000000000:microsservico-cliente-evento-pagamento-recusado-dlq\",\"maxReceiveCount\":\"3\"}" }'
+  --queue-name ms-cliente-evento-pagamento-recusado \
+  --attributes '{ "RedrivePolicy": "{\"deadLetterTargetArn\":\"arn:aws:sqs:us-east-1:000000000000:ms-cliente-evento-pagamento-recusado-dlq\",\"maxReceiveCount\":\"3\"}" }'
